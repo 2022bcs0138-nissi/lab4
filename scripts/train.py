d5ac60ff79215a22759d13bad89821f84f203cd6
2022bcs0138-nissi <nissi22bcs138@iiitkottayam.ac.in>
2026-01-23 12:37:25 +0530

    EXP-02: Train Ridge Regression model for comparison

--- scripts/train.py
+++ scripts/train.py
@@ -2,7 +2,7 @@ import json
 import joblib
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
 from sklearn.metrics import mean_squared_error, r2_score
 import os
 
@@ -18,7 +18,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-model = LinearRegression()
+model = Ridge(alpha=1.0)
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
